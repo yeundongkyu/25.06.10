b5ccd7a52fa3f2fb0a0c05efbbc78efa8a552f85
yeundongkyu <sn2025109@shs.hs.kr>
2025-06-10 14:47:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("나의 첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name==연동규:
+    st.success("반갑습니다. 홍길동님!")
+  else:
+    st.warning("누구세요?)
